d2b95bcf65576b15945ee22c3a56328ec97dd444
pravukalyandas <daspravukalyan555@gmail.com>
2024-05-29 22:38:43 +0530

    mapbox-sdk token added

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,5 +1,5 @@
 const Listing = require("../models/listing");
-const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mbxGeocoding = require('@mapbox/mapbox-sdk');
 const mapToken = process.env.MAP_TOKEN;
 const geocodingClient =mbxGeocoding({accessToken:mapToken});
 
